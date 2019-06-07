e631312fce382a5c69eab6c956e57439044626a9
kimyongjun413 <kimyongjun413@gmail.com>
2019-06-07 17:50:26 +0900

    파일 구조 변경

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# 자바로 푼 코드업 알고리즘 기초 100제
+# 자바로 푼 코드업 알고리즘
+
+### 기초100제
 
 ### 코드업 URL : http://codeup.kr/problemsetsol.php
---
+++ 기초5-4. 데이터 정렬/1172 세 수 정렬하기.java
@@ -0,0 +1,31 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] strNumber = scanner.nextLine().split(" ");
+        int[] numbers = new int[strNumber.length];
+        for (int i = 0; i < strNumber.length; i++) {
+            numbers[i] = Integer.parseInt(strNumber[i]);
+        }
+
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = numbers.length -1; j>i; j--) {
+                if(numbers[j] < numbers[j-1]) {
+                    swap(numbers, j, j-1);
+                }
+            }
+        }
+
+        for(int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void swap(int[] numbers, int j, int i) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+}
